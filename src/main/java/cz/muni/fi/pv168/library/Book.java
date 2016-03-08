b7274204cd7451dfa8dfa220f040d781da0fd4f1
Kristian Mateka <k1276.mateka@seznam.cz>
2016-03-08 16:08:29 +0100

    added setter - book

--- src/main/java/cz/muni/fi/pv168/library/Book.java
+++ src/main/java/cz/muni/fi/pv168/library/Book.java
@@ -19,11 +19,25 @@ public class Book {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public String getGenre() {
         return genre;
     }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
 }
