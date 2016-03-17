ab4e3a0c7ca4937eaffccba3c1e89a5e4df0d045
Lenka <lenka.janeckova88@gmail.com>
2016-03-17 20:09:41 +0100

    added equals, hashcode, toString to Book, Customer and Lease

--- src/main/java/cz/muni/fi/pv168/library/Lease.java
+++ src/main/java/cz/muni/fi/pv168/library/Lease.java
@@ -53,4 +53,31 @@ public class Lease {
     public void setEndTime(Date endTime) {
         this.endTime = endTime;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Lease lease = (Lease) o;
+
+        return id != null && id.equals(lease.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Lease: " +
+                "id=" + id +
+                ", " + book +
+                ", " + customer;// +
+                //", start time: " + startTime +
+                //", end time: " + endTime;
+    }
+
 }
